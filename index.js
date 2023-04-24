6eba02745e78c2cbd68d10000f471dff8b753fe0
ivel <ivelraj1997@gmail.com>
2023-04-24 13:56:19 +0530

    added port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const httpServer=require("http");
-
+const port = process.env.port || 3000;
 httpServer.createServer((req,res)=>{
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hello World!');
     res.end();
-}).listen(8080,()=>{
-    console.log("server listening on port 3000")
+}).listen(port,()=>{
+    console.log(`server listening on port ${port}`)
 })
